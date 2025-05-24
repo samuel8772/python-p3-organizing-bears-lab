60700e579268f90d94af24c669b51aa36681b234
samuel8772 <kamauskk005@gmail.com>
2025-05-24 18:35:24 +0300

    initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(id,name, age, sex, color, temperament, alive) VALUES
+(1,'Mr. Chocolate',8,'M','Brown','calm',1),
+(2,'Rowdy',8,'M','Brown','rowdy',1),
+(3,'Tabitha',5,'F','Black','shy',1),
+(4,'Sergeant Brown',6,'M','Brown','strict',1),
+(5,'Melissa',2,'F','Brown','calm',1),
+(6,'Grinch',7,'M','Brown','friendly',1),
+(7,'Wendy',4,'F','Brown','playful',1),
+(8,NULL, NULL,NULL,NULL,NULL,NULL);
